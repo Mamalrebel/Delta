1eba4b2b2be67f943392a0db4d58fa5b649313f9
mohamadrezahanji <mohamadrezahanji@gmail.com>
2022-01-04 00:42:03 +0330

    project added

---
+++ js/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    setTimeout(function() {
+        $('.loaderpos').hide();
+        $('.wrapper').show();
+    }, 2000);
+});
+
+$('.owl-carousel').owlCarousel({
+    autoplay:true,
+    autoplayTimeout:2000,
+    center:true,
+    rtl:true,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
